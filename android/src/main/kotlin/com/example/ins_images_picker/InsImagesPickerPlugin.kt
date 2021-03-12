0821a34a8fea9ace4d9c57c651743c4d8b5afb60
lecicdjuro <lecicdjuro@gmail.com>
2021-03-12 13:54:26 +0100

    lib version

--- android/src/main/kotlin/com/example/ins_images_picker/InsImagesPickerPlugin.kt
+++ android/src/main/kotlin/com/example/ins_images_picker/InsImagesPickerPlugin.kt
@@ -20,7 +20,6 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 
 /** InsImagesPickerPlugin */
@@ -37,22 +36,7 @@ class InsImagesPickerPlugin : FlutterPlugin, MethodCallHandler, OnResultCallback
         channel.setMethodCallHandler(this)
     }
 
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "ins_images_picker")
-            channel.setMethodCallHandler(InsImagesPickerPlugin())
-        }
-    }
+
 
     override fun getAppContext(): Context? {
         return activity
@@ -70,7 +54,7 @@ class InsImagesPickerPlugin : FlutterPlugin, MethodCallHandler, OnResultCallback
             }
             "pickerImages" -> {
                 InsGallery.applyInstagramOptions(activity!!.applicationContext, PictureSelector.create(activity)
-                        .openGallery(1))
+                        .openGallery( PictureMimeType.ofImage()))
                         .imageEngine(GlideEngine.createGlideEngine()).hideBottomControls(false).rotateEnabled(true).maxVideoSelectNum(0)
                         .isCamera(false).isCameraAroundState(false)
                         .selectionMode(PictureConfig.SINGLE).isEnableCrop(call.argument<Boolean>("showCrop")!!)
